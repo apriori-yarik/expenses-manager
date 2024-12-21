e57c85653cb20e55eda9c7a5cb5b9565d5cf142d
Yaroslav Dokuzov <yarik2803chess@yandex.ru>
2024-12-21 21:06:00 +0200

    The most basic background service ever ;)

---
+++ ExpensesManager/ExpensesManager.DomainServices/BackgroundServices/TestService.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpensesManager.DomainServices.BackgroundServices
+{
+    public class TestService : BackgroundService
+    {
+        private readonly ILogger _logger;
+
+        public TestService(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation("Hello world! Long-running background task working!");
+
+                await Task.Delay(3000, stoppingToken);
+            }
+
+            _logger.LogInformation("Background service stopped!");
+        }
+    }
+}
